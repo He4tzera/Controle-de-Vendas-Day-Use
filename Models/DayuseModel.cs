6abd878d600f621a9d5aba275baa8e384c41a3da
Eduardo <dudu_lima.11@hotmail.com>
2024-08-06 10:53:55 -0300

    style: Add new bootstrap style

--- Models/DayuseModel.cs
+++ Models/DayuseModel.cs
@@ -15,15 +15,15 @@ namespace Controle.Models
         [Required(ErrorMessage = "Por favor insira o telefone do cliente")]
         public string Telefone { get; set; }
 
-        [Required(ErrorMessage ="Por favor insira o numero da pulseira")]
+        [Required(ErrorMessage = "Por favor insira o numero da pulseira")]
         public int Pulseira { get; set; }
 
         public bool Aluno { get; set; }
 
         [Required(ErrorMessage = "Por favor insira o valor cobrado")]
-        public double Preco {  get; set; }
+        public double Preco { get; set; }
 
-    
+
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-
-
 using Controle.data;
 using Microsoft.EntityFrameworkCore;
 
