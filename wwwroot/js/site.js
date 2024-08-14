a151eb0969bcb42abc0b758232a35ab2b1282c6b
Eduardo <dudu_lima.11@hotmail.com>
2024-08-14 17:45:39 -0300

    Add New Layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,35 +1,48 @@
 ﻿$(document).ready(function () {
-    $('#Listagem').DataTable({
+    $('#lista').DataTable({
         language: {
-            "decimal": "",
-            "emptyTable": "No data available in table",
-            "info": "Mostrando _START_ de _END_ em um total de _TOTAL_ registros",
-            "infoEmpty": "Mostrando 0 to 0 of 0 entries",
-            "infoFiltered": "(filtered from _MAX_ total entries)",
-            "infoPostFix": "",
-            "thousands": ",",
-            "lengthMenu": "Mostrando _MENU_ registros por pagina",
-            "loadingRecords": "Loading...",
-            "processing": "",
-            "search": "Procurar:",
-            "zeroRecords": "Nenhum Registro Encontrado",
-            "paginate": {
-                "first": "Primeira",
-                "last": "Ultima",
-                "next": "Proxima",
-                "previous": "Anterior"
-            },
-            "aria": {
-                "orderable": "Order by this column",
-                "orderableReverse": "Reverse order this column"
-            }
+        "decimal": "",
+        "emptyTable": "Nenhum dado disponível na tabela",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+        "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+        "infoFiltered": "(filtrado de _MAX_ registros totais)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrando _MENU_ registros por página",
+        "loadingRecords": "Carregando...",
+        "processing": "Processando...",
+        "search": "Procurar:",
+        "zeroRecords": "Nenhum registro encontrado",
+        "paginate": {
+            "first": "Primeira",
+            "last": "Última",
+            "next": "Próxima",
+            "previous": "Anterior"
+        },
+        "aria": {
+            "sortAscending": ": ativar para ordenar a coluna de forma ascendente",
+            "sortDescending": ": ativar para ordenar a coluna de forma descendente"
         }
-    })
-
-
+    }
+});
     setTimeout(function () {
         $(".alert").fadeOut("slow", function () {
             $(this).alert('close');
-        })
+        });
     }, 2000)
+
 });
+
+function openDeleteModal(id) {
+    document.getElementById('deleteId').value = id;
+    var deleteModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+    deleteModal.show();
+}
+
+const hamBurger = document.querySelector(".toggle-btn");
+hamBurger.addEventListener("click", function () {
+    console.log("Hamburger clicked");
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
+
+
